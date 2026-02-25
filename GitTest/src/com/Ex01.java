9df347fd4a6a5125a69d1efa41511139b057fc23
LSH <tmdghksgkqru@naver.com>
2026-02-25 16:39:04 +0900

    승환 개인 브랜치 작업

--- GitTest/src/com/Ex01.java
+++ GitTest/src/com/Ex01.java
@@ -5,6 +5,12 @@ public class Ex01 {
 	public static void main(String[] args) {
 		System.out.println("팀장 커밋");
 		System.out.println("팀원1 두번째 커밋");
+		
+		
+		System.out.println("승환 개인 브랜치 커밋");
+		
+		
 	}
+	
 
 }
